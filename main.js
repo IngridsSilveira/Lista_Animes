85decfcd72dfcee3647a02d84be047a4ec385ec8
Ingrid Souza <92730473+IngridsSilveira@users.noreply.github.com>
2022-08-08 14:57:02 -0300

    adicionei allguns itens

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
-const lista = "https://my-json-server.typicode.com/ingridsSilveira/Lista_Animes/Animes";
+//ADICIONANDO OS ANIMES NA LISTA//
+function addAnime(){
+    let inputItens = {}
+    inputItens["nome"] = document.querySelector("[data-nome]").value;
 
+    fetch("https://my-json-server.typicode.com/ingridsSilveira/Lista_Animes/animes/post", {
+     
+    method: "POST",
+    body: JSON.stringify({inputItens}),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+})
+.then(response => response.json())
+
+.then(resposta =>  {
+    const nome = resposta.inputItens.nome;
+    document.querySelector(".item").innerHTML = nome;
+    console.log(nome);
+})
+pegandoData();
+}
+
+//PEGANDO OS DADOS DOS ANIMES//
 function pegandoData(){
-    fetch(lista).then((res) => res.json()
+    fetch('https://my-json-server.typicode.com/ingridsSilveira/Lista_Animes/animes').then((res) => res.json()
     ).then((resposta) => {
         var data = "";
         resposta.forEach((element) =>{
-            data += `<div class="anime">
-            <img src="${element.imagem}" alt="${element.nome}">
-            <h3>${element.nome}</h3>
-            <p>${element.sinopse}</p>
-            </div>`;
+            data += "<tr>"
+            data += "<td>"+element.nome+"</td>"
+            data += "<td><button class='btn'>Editar</button></td>"
+            data += "<td><button class='btn'>Deletar</button></td>"
+            data +="</tr>";
         })
+        document.querySelector("[data-corpo]").innerHTML = data;
     })
 }
 pegandoData();
