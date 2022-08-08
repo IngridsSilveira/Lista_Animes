06cd313d29b46fcbc846509fad2becde801fd6c3
Ingrid Souza <92730473+IngridsSilveira@users.noreply.github.com>
2022-08-08 11:41:57 -0300

    add dbJson

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-const lista = "db.json";
+const lista = "https://my-json-server.typicode.com/ingridsSilveira/Lista_Animes/Animes";
 
+function pegandoData(){
+    fetch(lista).then((res) => res.json()
+    ).then((resposta) => {
+        var data = "";
+        resposta.forEach((element) =>{
+            data += `<div class="anime">
+            <img src="${element.imagem}" alt="${element.nome}">
+            <h3>${element.nome}</h3>
+            <p>${element.sinopse}</p>
+            </div>`;
+        })
+    })
+}
+pegandoData();
