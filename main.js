f12c4d14d70295b4b73696f4502d81949e92a725
Ingrid Souza <92730473+IngridsSilveira@users.noreply.github.com>
2022-08-09 11:07:15 -0300

    js finalizado

--- main.js
+++ main.js
@@ -1,39 +1,94 @@
-//ADICIONANDO OS ANIMES NA LISTA//
-function addAnime(){
-    let inputItens = {}
-    inputItens["nome"] = document.querySelector("[data-nome]").value;
+const corpo = document.querySelector("[data-corpo]");
 
-    fetch("https://my-json-server.typicode.com/ingridsSilveira/Lista_Animes/animes/post", {
-     
+var url = 'http://localhost:3000/animes';
+
+let inputItens = document.querySelector("[data-nome]");
+
+let form = document.querySelector(".form");
+
+var data = "";
+
+//POST//ADICIONANDO OS ANIMES NA LISTA//
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    fetch(url, {
     method: "POST",
-    body: JSON.stringify({inputItens}),
     headers: {
         "Content-type": "application/json; charset=UTF-8"
-    }
-})
-.then(response => response.json())
-
-.then(resposta =>  {
-    const nome = resposta.inputItens.nome;
-    document.querySelector(".item").innerHTML = nome;
-    console.log(nome);
-})
-pegandoData();
-}
-
-//PEGANDO OS DADOS DOS ANIMES//
-function pegandoData(){
-    fetch('https://my-json-server.typicode.com/ingridsSilveira/Lista_Animes/animes').then((res) => res.json()
-    ).then((resposta) => {
-        var data = "";
-        resposta.forEach((element) =>{
-            data += "<tr>"
-            data += "<td>"+element.nome+"</td>"
-            data += "<td><button class='btn'>Editar</button></td>"
-            data += "<td><button class='btn'>Deletar</button></td>"
-            data +="</tr>";
-        })
-        document.querySelector("[data-corpo]").innerHTML = data;
+    },
+    body: JSON.stringify({
+        nome: inputItens.value,
     })
+    })
+    .then(response => response.json())
+    .then(resposta => {
+        const respostaArray = [];
+        respostaArray.push(resposta);
+        pegandoDados(respostaArray);
+    })
+})
+
+//GET//PEGANDO OS DADOS DOS ANIMES//
+    fetch(url).then(resposta => resposta.json())
+    .then(dados => pegandoDados(dados))
+        
+const pegandoDados = (dados) => {
+    dados.forEach(element => {
+        data += `
+            <tr data-id=${element.id}>
+            <td data-titulo>${element.nome}</td>
+            <td>
+            <button class='btn_editar' id="editar">Editar</button>
+            </td>
+            <td>
+            <button id="deletar" class="btn_deletar">Deletar</button>
+            </td>
+            </tr>
+            `
+    })
+    corpo.innerHTML = data;
 }
-pegandoData();
+
+corpo.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let botaoDeletar = e.target.id == 'deletar';
+    let botaoEditar = e.target.id == 'editar';
+
+    let id = e.target.parentElement.parentElement.dataset.id;
+
+//DELETANDO//
+    if(botaoDeletar){
+        fetch(`${url}/${id}`,{
+            method: "DELETE",
+        })
+        .then(resposta => resposta.json())
+        .then(() => location.reload())
+    }
+//SELECIONANDO O BOTÃO DE EDITAR E O CONTEUDO//
+    if(botaoEditar){
+      const parente = e.target.parentElement.parentElement;
+
+      let nomeConteudo = parente.querySelector("[data-titulo]").textContent;
+
+      inputItens.value = nomeConteudo;
+    }
+
+let btnEditar = document.querySelector(".btn_editar");
+
+//EDITANDO OS ITENS//
+btnEditar.addEventListener("click", () => {
+        fetch(`${url}/${id}`,{
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            },
+            body: JSON.stringify({
+                nome: inputItens.value,
+            })
+        })
+        .then(resposta => resposta.json())
+        .then(() => location.reload())
+    })
+})
